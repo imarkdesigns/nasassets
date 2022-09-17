9bab8644506486269007514036daf93d99638605
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-09-17 03:37:00 -0700

    Fix success stories

--- views/options/stories.php
+++ views/options/stories.php
@@ -20,47 +20,28 @@ if ( !is_home() ) {
                     <p>If you're looking for a better commercial real estate management company, put the NAS team to work for your asset today. You will quickly see how an experienced commercial property management company can make a real difference in the making of your property's success story.</p>
                 </div>
             </li>
-            <?php 
-            $title = [
-                'Crystal Lake Golf Club',
-                'Kings Cove Reopens',
-                'Gander Outdoors',
-                'Lorem Ipsum Dolor',
-                'Sit Amet Consectetur',
-                'Mauris Non Sagittis Nulla',
-            ];
+            <?php $home_stories = [ 'post_type' => 'nas-stories', 'posts_per_page' => 6, 'order' => 'ASC' ];
+            query_posts( $home_stories ); 
 
-            $address = [
-                'Deerfield Beach, FL',
-                'Kingwood, TX',
-                'Spring, TX',
-                'Los Angeles, CA',
-                'Syracuse, NY',
-                'Fort Worth, TX'
-            ];
-
-            $details = [
-                'Successful Property Re-purposing Strategy Delivers Buyer for Crystal Lake Golf Club.',
-                'Kings Cove was among the first multifamily properties to open after the devastation of Hurricane Harvey in Sept. 2017.  A $6 Million Property Renovation of the property was completed in March, 2018.',
-                'The Gander Outdoors building at a Houston-are freestanding big box retail location was leased in just three Months.',
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab inventore itaque quidem vero numquam explicabo vel aut iste, eveniet magni fuga iusto nihil minima et nesciunt odit. Quis, cupiditate, culpa!',
-                'Sed venenatis eros et neque eleifend, venenatis posuere sapien molestie. Maecenas at velit lobortis est tincidunt volutpat.',
-                'Ut eu quam nec erat tempor vestibulum sit amet at neque. Etiam ut urna vulputate, luctus nibh et, iaculis metus.'
-            ];
-
-            for ( $n=1;$n<=5;$n++ ) : ?>
+            while ( have_posts() ) : the_post(); ?>
             <li class="uk-width-1-2@s uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl">
                 <figure class="uk-inline">
-                    <img src="//placem.at/places?w=640&h=700&txt=0&random=30<?=$n?>" width="640" height="700" alt="">
+                    <?php 
+                    if ( has_post_thumbnail($post->ID) ) {
+                        $featuredID = get_post_thumbnail_id($post->ID);
+                        echo wp_get_attachment_image( $featuredID, 'large' );
+                    } else {
+                        echo '<img src="//placem.at/places?w=640&h=700&txt=0&random=30'.$post->ID.'" width="640" height="700" alt="">';
+                    } ?>                    
                     <figcaption class="uk-overlay-primary uk-position-bottom uk-padding">
-                        <span class="story-address"><?php echo $address[$n]; ?></span>
-                        <h3 class="story-title"><?php echo $title[$n]; ?></h3>
-                        <p><?php echo $details[$n]; ?></p>
+                        <span class="story-address"><?php the_field( 'ss_location' ); ?></span>
+                        <h3 class="story-title"><?php the_title(); ?></h3>
+                        <p><?php the_field( 'ss_lead' ); ?></p>
                         <p><a href="#" class="uk-button uk-button-primary">Read More</a></p>
                     </figcaption>
                 </figure>
             </li>
-            <?php endfor; ?>
+            <?php endwhile; wp_reset_query(); ?>
         </ul>
 
         <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" role="presentation" aria-label="Previous" uk-slidenav-previous uk-slider-item="previous"></a>
